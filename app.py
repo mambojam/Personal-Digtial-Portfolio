157fa7f9529dd4a791a5658215653a7e7c243fe6
Harry Jacobs <jacobsh2@cardiff.ac.uk>
2023-12-21 12:01:48 +0000

    adjusted the projects list templating to generate a link based off the html key in the project dictionary. Started creating the front end for the online store with a filter form which will be used to generate SQL queries and return filtered shop items

--- app.py
+++ app.py
@@ -2,35 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects_data = [
+    
+        {   'html': 'store.html',
+            'title': 'Online Store',
+            'description': 'When I first started learning SQL, I set up a simple DB for online outdoor clothes store. I\'ve now design and integrated a front end for it.',
+            'image': 'static/project1.jpg'
+        },
+        {   
+            'html': 'game.html',
+            'title': 'Project 2',
+            'description': 'Description of Project 2.',
+            'image': 'static/project2.jpg'
+        }
+
+]
+
 @app.route('/')
 def home():
     return render_template('layout.html')
 
 @app.route('/projects')
 def projects():
-    projects_data = [
-        {
-            'id': 1,
-            'title': 'Online Store',
-            'description': 'When I first started learning SQL, I set up a simple DB for online outdoor clothes store. I\'ve now design and integrated a front end for it.',
-            'image': 'static/project1.jpg'
-        },
-        {
-            'id': 2,
-            'title': 'Project 2',
-            'description': 'Description of Project 2.',
-            'image': 'static/project2.jpg'
-        },
-        # Add more projects as needed
-    ]
     return render_template('projects.html', projects=projects_data)
 
-@app.route('/project/<int:project_id>')
-def project_detail(project_id):
-    return "Hello_world"
-
-
-
+@app.route('/project/<string:html_file>')
+def project_detail(html_file):
+    return render_template(html_file)
 
 if __name__ == '__main__':
     app.run(debug=True)
