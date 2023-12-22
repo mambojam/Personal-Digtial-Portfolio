cde37098392b5f472b8834c6b94c77e123d01210
Harry Jacobs <jacobsh2@cardiff.ac.uk>
2023-12-22 16:33:03 +0000

    got the form extracting all the right data including a list for categories. started working on the routes to select corresponding data from the DB, but they're not working properly yet

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -29,6 +30,40 @@ def projects():
 @app.route('/project/<string:html_file>')
 def project_detail(html_file):
     return render_template(html_file)
+
+@app.route('/test')
+def testSearch():
+    conn = sqlite3.connect('Online_Store_DB.db')
+    cur = conn.cursor()
+    query = "SELECT * FROM Product"
+    res = str(conn.execute(query))
+    print(res)
+    return res
+
+# need to watch a tutorial on this as I'm getting stuck
+# how to write a select query to return the data and add to the html
+
+@app.route('/searchItems', methods=['POST'])
+def loadItems():
+    categories = request.form['categories']
+    minPrice = request.form['minPrice']
+    maxPrice = request.form['maxPrice']
+    try:
+        conn = sqlite3.connect('Online_Store_DB.db')
+        cur = conn.cursor()
+        query = "SELECT * FROM Products "
+        for num in range(len(categories)):
+            if num == 0:
+                query += "WHERE "
+            else:
+                query += "OR "
+            query += "category = \"" + categories[num] + "\" AND " + int(minPrice) + " < price AND price < " + int(maxPrice)
+        res = cur.execute(query)
+    except:
+	    res = "error in finding items"
+    finally:
+        conn.close()
+        return msg
 
 if __name__ == '__main__':
     app.run(debug=True)
