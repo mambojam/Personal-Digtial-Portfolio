8f72f10fa63cbb1111fd368026a2b9c3cb19eb71
Harry Jacobs <jacobsh2@cardiff.ac.uk>
2023-12-28 17:14:33 +0000

    sorted out the styling for the products in the store, set default params for the filter, and worked on getting images(BLOB) into and back from the databse - not displaying properly yet though

--- app.py
+++ app.py
@@ -33,18 +33,6 @@ def projects():
 def project_detail(html_file):
     return render_template(html_file)
 
-@app.route('/test')
-def testSearch():
-    conn = sqlite3.connect('Online_Store_DB.db')
-    cur = conn.cursor()
-    query = "SELECT * FROM Product"
-    res = str(conn.execute(query))
-    print(res)
-    return res
-
-# need to watch a tutorial on this as I'm getting stuck
-# how to write a select query to return the data and add to the html
-
 @app.route('/searchItems', methods=['POST'])
 def loadItems():
     categories = json.loads(request.form['categories']) # turn the JSON string back into a list
@@ -55,16 +43,6 @@ def loadItems():
         conn = sqlite3.connect('Online_Store_DB.db')
         cur = conn.cursor()
 
-        # query = "SELECT * FROM Products "
-        # for num in range(len(categories)):
-        #     if num == 0:
-        #         query += "WHERE "
-        #     else:
-        #         query += "OR "
-        #     query += "category = \"" + categories[num] + "\" "
-        # query += "AND price BETWEEN (%s) and (%s)", [minPrice, maxPrice]    
-    
-
         # create the query 
         query = "SELECT * FROM Product WHERE "
         category_conditions = " OR ".join(["category = ?" for _ in categories])
@@ -73,18 +51,31 @@ def loadItems():
         res = cur.execute(query, [min_price, max_price] + categories)
         print(res)
         productlist = res.fetchall()
+        # Get column names dynamically
+        columns = [column[0] for column in cur.description]
+        # Convert the list of tuples to a list of dictionaries
+        all_products = [dict(zip(columns, row)) for row in productlist]
 
     except sqlite3.Error as e:
 	    logging.error(f"SQLite Error: {e}")
         
     finally:
         conn.close()
-        return jsonify(productlist)
-        # return jsonify({'htmlresponse': render_template('response.html', productlist=productlist)})
+        return jsonify(all_products)
+
+@app.route('/test', methods=['GET'])
+def getimages():
+    conn = sqlite3.connect('Online_Store_DB.db')
+    cur = conn.cursor()
+    query = "SELECT * FROM Images"
+    res = cur.execute(query)
+    productlist = res.fetchall()
+    return jsonify(productlist)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-## getting this to return a list of items now - it's not the full list for some reason - need to work on
-## making sure the querty is correct, then can look into adding images, and displaying the content properly
+## need to add images and work on the display next, also need to take the underscores out of names and swap for spaces
+## need to set default params
---
+++ insert_blob.py
@@ -0,0 +1,68 @@
+import sqlite3
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        blobData = file.read()
+    return blobData
+
+def insertBLOB(data_tuple):
+    try:
+        conn = sqlite3.connect('Online_Store_DB.db')
+        cur = conn.cursor()
+        print("Connected to SQLite")
+        sqlite_insert_blob_query = """ INSERT INTO Product
+                                  (product_name, category, price, image) VALUES (?, ?, ?, ?)"""
+
+        prod_image = convertToBinaryData(data_tuple[3])
+        
+        cur.execute(sqlite_insert_blob_query, data_tuple)
+        conn.commit()
+        print("Image and file inserted successfully as a BLOB into a table")
+        cur.close()
+
+    except sqlite3.Error as error:
+        print("Failed to insert blob data into sqlite table", error)
+    finally:
+        if conn:
+            conn.close()
+            print("the sqlite connection is closed")
+
+
+values = [
+('Scarpa_Quantix_SF', 'Rock_Shoe', 105.00, 'static/images/shoe.jpg'),
+('Scarpa_Mago', 'Rock_Shoe', 99.00, 'static/images/shoe.jpg'),
+('La_Sportiva_TC_Pro', 'Rock_Shoe', 155.00, 'static/images/shoe.jpg'),
+('La_Sportiva_Skwama', 'Rock_Shoe', 130.00, 'static/images/shoe.jpg'),
+('La_Sportiva_Miura_Lace', 'Rock_Shoe', 127.00, 'static/images/shoe.jpg'),
+('Mammut_9.5MM_50M', 'Rope', 112.00, 'static/images/rope.jpg'),
+('Edelweiss_9.2MM_70M', 'Rope', 212.00, 'static/images/rope.jpg'),
+('Edelrid_Eco_50M', 'Rope', 157.50, 'static/images/rope.jpg'),
+('Beal_Phantom', 'Harness', 54.00, 'static/images/harness.jpg'),
+('DMM_Centre', 'Harness', 58.50, 'static/images/harness.jpg'),
+('DMM_Centre', 'Harness', 58.50, 'static/images/harness.jpg'),
+('Ocun_Webee_Bigwall', 'Harness', 85.50, 'static/images/harness.jpg'),
+('Mammut_Bionic_5_Pack', 'Quickdraw', 82.50, 'static/images/quickdraw.jpg'),
+('Black_Diamond_6_Pack', 'Quickdraw', 85.50, 'static/images/quickdraw.jpg'),
+('DMM_Alpha_6_Pack', 'Quickdraw', 128.00, 'static/images/quickdraw.jpg')
+]
+
+for value in values:
+    insertBLOB(value)
+
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
+# insertBLOB('Scarpa_Vapour_S', 'Rock_Shoe', 125.00, 'static/images/shoe.jpg')
