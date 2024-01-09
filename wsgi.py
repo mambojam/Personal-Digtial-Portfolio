e126cbae210d9383d122d50367b61071979dc60e
Harry Jacobs <jacobsh2@cardiff.ac.uk>
2024-01-09 22:04:29 +0000

    renamed app.py > portfolio.py

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@
 # logging.basicConfig(stream=sys.stderr)
 # sys.path.insert(0,"/root/customer-account-automation/")
 
-from app import app as application
+from portfolio import app as application
 # if __name__ == "__main__":
-#     app=application
+#     app=applicationgit add .
